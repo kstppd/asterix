43eac5a44612f08807b11680b950cdda299ecdc2
Markus Battarbee <markus.battarbee@gmail.com>
2024-06-11 12:39:54 +0300

    Batch loop reduction of content blocks from map to list

--- spatial_batch_gpu.cpp
+++ spatial_batch_gpu.cpp
@@ -135,6 +135,103 @@ __global__ void batch_reset_all_to_empty(
    return;
 }
 
+/*
+ * Extracts keys from all provided hashmaps to provided splitvectors, and stores the vector size in an array.
+ */
+__global__ void extract_all_content_blocks(
+   Hashinator::Hashmap<vmesh::GlobalID,vmesh::LocalID>**maps, //we're going to access only every second entry (content, not no-content)
+   split::SplitVector<vmesh::GlobalID> **outputVecs,
+   vmesh::LocalID* dev_contentSizes,
+   vmesh::GlobalID emptybucket,
+   vmesh::GlobalID tombstone
+   ) {
+   //launch parameters: dim3 grid(nMaps,1,1); // As this is a looping reduction
+   const size_t hashmapIndex = blockIdx.x;
+   Hashinator::Hashmap<vmesh::GlobalID,vmesh::LocalID>* thisMap = maps[hashmapIndex*2];
+   split::SplitVector<vmesh::GlobalID> *outputVec = outputVecs[hashmapIndex];
+
+   // This must be equal to at least both WARPLENGTH and MAX_BLOCKSIZE/WARPLENGTH
+   __shared__ uint32_t warpSums[WARPLENGTH];
+   __shared__ uint32_t outputCount;
+   // blockIdx.x is always 0 for this kernel
+   const size_t tid = threadIdx.x; // + blockIdx.x * blockDim.x;
+   const size_t wid = tid / WARPLENGTH;
+   const size_t w_tid = tid % WARPLENGTH;
+   //const uint warpsPerBlock = BLOCKSIZE / WARPLENGTH;
+   const uint warpsPerBlock = blockDim.x / WARPLENGTH;
+   // zero init shared buffer
+   if (wid == 0) {
+      warpSums[w_tid] = 0;
+   }
+   __syncthreads();
+   // full warp votes for rule-> mask = [01010101010101010101010101010101]
+   int64_t remaining = thisMap->bucket_count();
+   const uint capacity = outputVec->capacity();
+   uint32_t outputSize = 0;
+   // Initial pointers into data
+   Hashinator::hash_pair<vmesh::GlobalID, vmesh::LocalID> *input = thisMap->expose_bucketdata<false>();
+   vmesh::GlobalID* output = outputVec->data();
+   // Start loop
+   while (remaining > 0) {
+      int current = remaining > blockDim.x ? blockDim.x : remaining;
+      __syncthreads();
+      const int active = (tid < current) ? (input[tid].first != tombstone && input[tid].first != emptybucket) : false;
+      const auto mask = split::s_warpVote(active == 1, SPLIT_VOTING_MASK);
+      const auto warpCount = split::s_pop_count(mask);
+      if (w_tid == 0) {
+         warpSums[wid] = warpCount;
+      }
+      __syncthreads();
+      // Figure out the total here because we overwrite shared mem later
+      if (wid == 0) {
+         // ceil int division
+         int activeWARPS = nextPow2(1 + ((current - 1) / WARPLENGTH));
+         auto reduceCounts = [activeWARPS](int localCount) -> int {
+            for (int i = activeWARPS / 2; i > 0; i = i / 2) {
+               localCount += split::s_shuffle_down(localCount, i, SPLIT_VOTING_MASK);
+            }
+            return localCount;
+         };
+         auto localCount = warpSums[w_tid];
+         int totalCount = reduceCounts(localCount);
+         if (w_tid == 0) {
+            outputCount = totalCount;
+            outputSize += totalCount;
+            assert((outputSize <= capacity) && "loop_compact ran out of capacity!");
+            outputVec->device_resize(outputSize);
+         }
+      }
+      // Prefix scan WarpSums on the first warp
+      if (wid == 0) {
+         auto value = warpSums[w_tid];
+         for (int d = 1; d < warpsPerBlock; d = 2 * d) {
+            int res = split::s_shuffle_up(value, d, SPLIT_VOTING_MASK);
+            if (tid % warpsPerBlock >= d) {
+               value += res;
+            }
+         }
+         warpSums[w_tid] = value;
+      }
+      __syncthreads();
+      auto offset = (wid == 0) ? 0 : warpSums[wid - 1];
+      auto pp = split::s_pop_count(mask & ((ONE << w_tid) - ONE));
+      const auto warpTidWriteIndex = offset + pp;
+      if (active) {
+         output[warpTidWriteIndex] = input[tid].first;
+      }
+      // Next loop iteration:
+      input += current;
+      output += outputCount;
+      remaining -= current;
+   }
+   __syncthreads();
+   if (tid == 0) {
+      // Resize to final correct output size.
+      outputVec->device_resize(outputSize);
+      dev_contentSizes[hashmapIndex] = outputSize;
+   }
+}
+
 
 /** Bulk call over listed cells of spatial grid
     Prepares the content / no-content velocity block lists
@@ -157,26 +254,32 @@ __global__ void batch_reset_all_to_empty(
       const gpuStream_t baseStream = gpu_getStream();
 
       // Allocate buffers for GPU operations
+      phiprof::Timer mallocTimer {"allocate buffers for content list analysis"};
       Hashinator::Hashmap<vmesh::GlobalID,vmesh::LocalID>** host_allMaps, **dev_allMaps;
+      split::SplitVector<vmesh::GlobalID> ** host_vbwcl_vec, **dev_vbwcl_vec;
       //size_t* host_sizePowers, *dev_sizePowers;
       Real* host_minValues, *dev_minValues;
       vmesh::VelocityMesh** host_vmeshes, **dev_vmeshes;
       vmesh::VelocityBlockContainer** host_VBCs, **dev_VBCs;
-      //vmesh::LocalID* host_contentSizes, *dev_contentSizes;
+      vmesh::LocalID* host_contentSizes, *dev_contentSizes;
       CHK_ERR( gpuMallocHost((void**)&host_allMaps, 2*nCells*sizeof(Hashinator::Hashmap<vmesh::GlobalID,vmesh::LocalID>*)) );
+      CHK_ERR( gpuMallocHost((void**)&host_vbwcl_vec, nCells*sizeof(split::SplitVector<vmesh::GlobalID>*)) );
       //CHK_ERR( gpuMallocHost((void**)&host_sizePowers, 2*nCells*sizeof(size_t)) );
       CHK_ERR( gpuMallocHost((void**)&host_minValues, nCells*sizeof(Real)) );
       CHK_ERR( gpuMallocHost((void**)&host_vmeshes,nCells*sizeof(vmesh::VelocityMesh*)) );
       CHK_ERR( gpuMallocHost((void**)&host_VBCs,nCells*sizeof(vmesh::VelocityBlockContainer*)) );
-      // CHK_ERR( gpuMallocHost((void**)&host_contentSizes,nCells*sizeof(vmesh::LocalID)) );
-      // CHK_ERR( gpuMallocAsync((void**)&dev_contentSizes,nCells*sizeof(vmesh::LocalID),baseStream) );
+      CHK_ERR( gpuMallocHost((void**)&host_contentSizes,nCells*sizeof(vmesh::LocalID)) );
+      CHK_ERR( gpuMallocAsync((void**)&dev_contentSizes,nCells*sizeof(vmesh::LocalID),baseStream) );
       CHK_ERR( gpuMallocAsync((void**)&dev_allMaps, 2*nCells*sizeof(Hashinator::Hashmap<vmesh::GlobalID,vmesh::LocalID>*),baseStream) );
+      CHK_ERR( gpuMallocAsync((void**)&dev_vbwcl_vec, nCells*sizeof(split::SplitVector<vmesh::GlobalID>*),baseStream) );
       //CHK_ERR( gpuMallocAsync((void**)&dev_sizePowers, 2*nCells*sizeof(size_t),baseStream) );
       CHK_ERR( gpuMallocAsync((void**)&dev_minValues,nCells*sizeof(Real),baseStream) );
       CHK_ERR( gpuMallocAsync((void**)&dev_vmeshes,nCells*sizeof(vmesh::VelocityMesh*),baseStream) );
       CHK_ERR( gpuMallocAsync((void**)&dev_VBCs,nCells*sizeof(vmesh::VelocityBlockContainer*),baseStream) );
+      CHK_ERR( gpuStreamSynchronize(baseStream) );
+      mallocTimer.stop();
 
-      phiprof::Timer sparsityTimer {"update Sparsity values and clear maps"};
+      phiprof::Timer sparsityTimer {"update Sparsity values"};
       size_t largestSizePower = 0;
       size_t largestVelMesh = 0;
 #pragma omp parallel
@@ -200,10 +303,12 @@ __global__ void batch_reset_all_to_empty(
 
             // Store values and pointers
             host_vmeshes[i] = SC->dev_get_velocity_mesh(popID);
+
             host_VBCs[i] = SC->dev_get_velocity_blocks(popID);
             host_minValues[i] = SC->getVelocityBlockMinValue(popID);
             host_allMaps[2*i  ] = SC->dev_velocity_block_with_content_map;
             host_allMaps[2*i+1] = SC->dev_velocity_block_with_no_content_map;
+            host_vbwcl_vec[i] = SC->dev_velocity_block_with_content_list;
             //host_sizePowers[2*i  ] = SC->vbwcl_sizePower;
             //host_sizePowers[2*i+1] = SC->vbwncl_sizePower;
 
@@ -221,8 +326,10 @@ __global__ void batch_reset_all_to_empty(
       }
       sparsityTimer.stop();
 
+      phiprof::Timer copyTimer {"copy values to device"};
       // Copy pointers and counters over to device
       CHK_ERR( gpuMemcpyAsync(dev_allMaps, host_allMaps, 2*nCells*sizeof(Hashinator::Hashmap<vmesh::GlobalID,vmesh::LocalID>*), gpuMemcpyHostToDevice, baseStream) );
+      CHK_ERR( gpuMemcpyAsync(dev_vbwcl_vec, host_vbwcl_vec, nCells*sizeof(split::SplitVector<vmesh::GlobalID>*), gpuMemcpyHostToDevice, baseStream) );
       //CHK_ERR( gpuMemcpyAsync(dev_sizePowers, host_sizePowers, 2*nCells*sizeof(size_t), gpuMemcpyHostToDevice, baseStream) );
       CHK_ERR( gpuMemcpyAsync(dev_minValues, host_minValues, nCells*sizeof(Real), gpuMemcpyHostToDevice, baseStream) );
       CHK_ERR( gpuMemcpyAsync(dev_vmeshes, host_vmeshes, nCells*sizeof(vmesh::VelocityMesh*), gpuMemcpyHostToDevice, baseStream) );
@@ -234,10 +341,18 @@ __global__ void batch_reset_all_to_empty(
       const size_t blocksNeeded = largestMapSize / Hashinator::defaults::MAX_BLOCKSIZE + (largestMapSize % Hashinator::defaults::MAX_BLOCKSIZE != 0);
       dim3 grid1(2*nCells,blocksNeeded,1);
       const vmesh::GlobalID emptybucket = host_allMaps[0]->expose_emptybucket();
+      const vmesh::GlobalID tombstone = host_allMaps[0]->expose_tombstone();
       //std::cerr<<"launch "<<2*nCells<<" "<<blocksNeeded<<" "<<Hashinator::defaults::MAX_BLOCKSIZE<<" "<<emptybucket<<std::endl;
       CHK_ERR( gpuStreamSynchronize(baseStream) );
-      batch_reset_all_to_empty<<<grid1, Hashinator::defaults::MAX_BLOCKSIZE, 0, baseStream>>>(dev_allMaps,emptybucket);
+      copyTimer.stop();
+      phiprof::Timer clearTimer {"clear all content maps"};
+      batch_reset_all_to_empty<<<grid1, Hashinator::defaults::MAX_BLOCKSIZE, 0, baseStream>>>(
+         dev_allMaps,
+         emptybucket
+         );
+      CHK_ERR( gpuPeekAtLastError() );
       CHK_ERR( gpuStreamSynchronize(baseStream) );
+      clearTimer.stop();
 
       // Batch gather GID-LID-pairs into two maps
       phiprof::Timer blockKernelTimer {"update content lists kernel 1"};
@@ -245,40 +360,61 @@ __global__ void batch_reset_all_to_empty(
       // ceil int division
       const uint launchBlocks = 1 + ((largestVelMesh - 1) / vlasiBlocksPerWorkUnit);
       dim3 grid2(nCells,launchBlocks,1);
-
-      batch_update_velocity_block_content_lists_kernel<<<grid2, (vlasiBlocksPerWorkUnit * WID3), 0, 0>>> (
+      batch_update_velocity_block_content_lists_kernel<<<grid2, (vlasiBlocksPerWorkUnit * WID3), 0, baseStream>>> (
          dev_vmeshes,
          dev_VBCs,
          dev_allMaps,
          dev_minValues
          );
       CHK_ERR( gpuPeekAtLastError() );
-      CHK_ERR( gpuDeviceSynchronize() );
+      CHK_ERR( gpuStreamSynchronize(baseStream) );
       blockKernelTimer.stop();
 
+
+      // Extract all keys
+      phiprof::Timer extractKeysTimer {"extract content keys"};
+      // auto rule = [] __host__ __device__(const Hashinator::hash_pair<vmesh::GlobalID, vmesh::LocalID>& kval) -> bool {
+      //    return kval.first != emptybucket && kval.first != tombstone;
+      // };
+      extract_all_content_blocks<<<nCells, Hashinator::defaults::MAX_BLOCKSIZE, 0, baseStream>>>(
+         dev_allMaps,
+         dev_vbwcl_vec,
+         dev_contentSizes,
+         emptybucket,
+         tombstone
+         );
+      CHK_ERR( gpuPeekAtLastError() );
+      CHK_ERR( gpuStreamSynchronize(baseStream) );
+      extractKeysTimer.stop();
+
+
       phiprof::Timer blocklistTimer {"update content lists extract"};
+      CHK_ERR( gpuMemcpyAsync(host_contentSizes, dev_contentSizes, nCells*sizeof(vmesh::LocalID), gpuMemcpyDeviceToHost, baseStream) );
+      CHK_ERR( gpuStreamSynchronize(baseStream) );
 #pragma omp parallel
       {
-         const gpuStream_t stream = gpu_getStream();
-         split::SplitInfo info;
+         // const gpuStream_t stream = gpu_getStream();
+         // split::SplitInfo info;
          SpatialCell *SC;
 #pragma omp for
          for (uint i=0; i<nCells; ++i) {
             SC = mpiGrid[cells[i]];
+            SC->velocity_block_with_content_list_size = host_contentSizes[i];
             //SC->update_velocity_block_content_lists(popID);
 
             // Now extract values from the map
-            SC->velocity_block_with_content_map->extractAllKeysLoop(*(SC->dev_velocity_block_with_content_list),stream);
+            //SC->velocity_block_with_content_map->extractAllKeysLoop(*(SC->dev_velocity_block_with_content_list),stream);
             // split::SplitInfo info;
             // Hashinator::MapInfo info_m;
             // TODO: in batch extraction loop, store end size in batch array
-            SC->velocity_block_with_content_list->copyMetadata(&info, stream);
-            CHK_ERR( gpuStreamSynchronize(stream) );
-            SC->velocity_block_with_content_list_size = info.size;
+            // SC->velocity_block_with_content_list->copyMetadata(&info, stream);
+            // CHK_ERR( gpuStreamSynchronize(stream) );
+            //SC->velocity_block_with_content_list_size = info.size;
             //velocity_block_with_content_list_size = velocity_block_with_content_list->size();
          }
       }
       blocklistTimer.stop();
+
    }
 
    //CHK_ERR( gpuMemcpy(host_contentSizes, dev_contentSizes, nCells*sizeof(vmesh::LocalID), gpuMemcpyDeviceToHost) );
