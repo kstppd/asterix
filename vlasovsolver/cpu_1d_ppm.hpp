5ce2b8726651e716ef053f308d70b3af5151a19c
Markus Battarbee <markus.battarbee@gmail.com>
2024-12-17 10:14:39 +0200

    Change slope limiters to take reference instead of Vec copy. Set many Vecs to const.

--- vlasovsolver/cpu_1d_ppm.hpp
+++ vlasovsolver/cpu_1d_ppm.hpp
@@ -36,13 +36,11 @@ using namespace std;
   Compute parabolic reconstruction with an explicit scheme
 */
 inline void compute_ppm_coeff(const Vec * const values, face_estimate_order order, uint k, Vec a[3], const Realv threshold){
-   Vec fv_l; /*left face value*/
-   Vec fv_r; /*right face value*/
-   compute_filtered_face_values(values, k, order, fv_l, fv_r, threshold); 
+   Vec m_face; /*left face value*/
+   Vec p_face; /*right face value*/
+   compute_filtered_face_values(values, k, order, m_face, p_face, threshold); 
    
    //Coella et al, check for monotonicity   
-   Vec m_face = fv_l;
-   Vec p_face = fv_r;
    m_face = select((p_face - m_face) * (values[k] - 0.5 * (m_face + p_face)) >
                    (p_face - m_face)*(p_face - m_face) * one_sixth,
                    3 * values[k] - 2 * p_face,
