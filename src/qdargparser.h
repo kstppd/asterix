7f67ba13ef80c9742c83098c5c4a786e76ed82bf
Konstantinos Papadakis <kpapadakis@protonmail.com>
2025-03-10 17:10:50 +0200

    update parser for examples

--- src/qdargparser.h
+++ src/qdargparser.h
@@ -1,34 +1,25 @@
-/*
-qdargparser.h
+/* File: qdargparser.h
 A sub 100LOC quick and dirty parser.
 It's not great code but works!
 Author: Kostis Papadakis, 2025 (kpapadakis@protonmail.com)
 Usage:
   constexpr auto delim=' '; //for no delim use '0'
   QdArgParser<delim> cli_args(argc, argv);
-  for (auto it=cli_args.begin(); it!=cli_args.end(); it=it.next()){
+  for (auto it=cli_args.begin(); it!=cli_args.end();++it){
     std::cout<<*it<<"\n";
   }
-  
-Have a look at:
-  std::vector<T> apply_collect_per_element(F &&functor);
-  template <typename F> auto apply(F &&functor);
-
 This program is free software; you can redistribute it and/or
 modify it as you wish. You can make money with it or scam people.
 */
-#include <algorithm>
 #include <cstring>
 #include <iostream>
 #include <iterator>
 #include <stdlib.h>
-#include <string>
 #include <string_view>
 #include <vector>
-
-template <char _delim> struct QdArgParser {
-  std::vector<char> _data;
-  std::string data;
+template <char _delim, typename Allocator = std::allocator<char>>
+struct QdArgParser {
+  std::vector<char, Allocator> _data;
   QdArgParser(int argc, char **buffer) {
     if (argc > 0) {
       for (int i = 0; i < argc; ++i) {
@@ -39,7 +30,6 @@ template <char _delim> struct QdArgParser {
       _data.pop_back();
     }
   }
-
   QdArgParser(const char *buffer, std::size_t size) {
     if (size > 0) {
       _data.insert(_data.end(), buffer, buffer + size + 1);
@@ -47,32 +37,32 @@ template <char _delim> struct QdArgParser {
       _data.pop_back();
     }
   }
-
-  template <typename F> auto apply(F &&functor) const {
+  template <typename F> auto apply(F &&functor) const noexcept {
     return functor(_data.begin(), _data.end());
   }
-
   template <typename T, typename F>
-  std::vector<T> apply_collect_per_element(F &&functor) {
+  std::vector<T> apply_collect_per_element(F &&functor) noexcept {
     std::vector<T> vals;
     for (auto it = begin(); it != end(); it = it.next()) {
       vals.push_back(functor(*it));
     }
     return vals;
   }
-
+  void print() noexcept {
+    std::cout << "{";
+    for (auto it = begin(); it != end(); ++it) {
+      std::cout << *it << ", ";
+    }
+    std::cout << "}\n";
+  }
   struct Iterator {
+    using iterator_category = std::forward_iterator_tag;
     std::string_view view;
     char *_pos;
-    std::size_t shift;
-    std::size_t left_over;
+    std::size_t shift={0};
+    std::size_t left_over={0};
     Iterator(char *pos, std::size_t len) : _pos(pos) {
-      char *it;
-      if constexpr (_delim == '0') {
-        it = pos + 1;
-      } else {
-        it = std::find(pos, pos + len, _delim);
-      }
+      const char *it = (_delim == '0') ? pos + 1 : std::find(pos, pos + len, _delim);
       if (it == pos + len) {
         view = std::string_view(pos, pos + len);
         left_over = 0;
@@ -84,16 +74,23 @@ template <char _delim> struct QdArgParser {
       }
     }
     std::string_view operator*() { return view; }
-    bool operator==(const Iterator &other) const { return _pos == other._pos; }
+    char *operator->() { return view.data(); }
+    bool operator==(const Iterator &other) const noexcept {
+      return _pos == other._pos;
+    }
+    bool operator!=(const Iterator &other) const noexcept {
+      return !(_pos == other._pos);
+    }
     char *data() { return _pos; }
     Iterator next() {
-      if constexpr (_delim == '0') {
-        return Iterator(_pos + shift, left_over);
-      } else {
-        return Iterator(_pos + shift + 1 * (left_over > 0), left_over);
-      }
+      return (_delim == '0')
+                 ? Iterator(_pos + shift, left_over)
+                 : Iterator(_pos + shift + 1 * (left_over > 0), left_over);
+    }
+    Iterator &operator++() {
+      return *this=next(),*this;
     }
   };
-  Iterator begin() { return Iterator(&_data[0], _data.size()); }
-  Iterator end() { return Iterator(&_data[0] + _data.size(), 0); }
+  Iterator begin() { return Iterator(_data.data(), _data.size()); }
+  Iterator end() { return Iterator(_data.data() + _data.size(), 0); }
 };
