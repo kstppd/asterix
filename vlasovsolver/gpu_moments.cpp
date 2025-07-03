b7307af70557dbb3d6cfa3db91b49223f645b2e7
Mikael Huppunen <mikael.huppunen@gmail.com>
2025-07-03 13:52:59 +0300

    Optimized second moments kernel

--- vlasovsolver/gpu_moments.cpp
+++ vlasovsolver/gpu_moments.cpp
@@ -141,19 +141,22 @@ __global__ void second_moments_kernel (
    const vmesh::VelocityBlockContainer* __restrict__ const *dev_VBC,
    const Real* __restrict__ dev_moments1,
    Real* dev_moments2,
-   const uint nAllCells)
+   const uint nAllCells,
+   const uint wid3PerBlockSecondMoments)
 {
+   const uint wid3Index = threadIdx.z/WID;
+
    const uint celli = blockIdx.x; // used for pointer to cell
-   const uint stride = gridDim.y; // used for faster looping over contents
-   const uint strideOffset = blockIdx.y; // used for faster looping over contents
+   const uint stride = gridDim.y*wid3PerBlockSecondMoments; // used for faster looping over contents
+   const uint strideOffset = blockIdx.y*wid3PerBlockSecondMoments + wid3Index; // used for faster looping over contents
    const uint ti = threadIdx.z*blockDim.x*blockDim.y + threadIdx.y*blockDim.x + threadIdx.x;
    const int i = threadIdx.x;
    const int j = threadIdx.y;
-   const int k = threadIdx.z;
+   const int k = threadIdx.z%WID;
    const int blockSize = blockDim.x*blockDim.y*blockDim.z;
 
    Real myMom[nMom2] = {0};
-   __shared__ Real smom[WID3];
+   extern __shared__ Real smom[];
 
    const vmesh::VelocityBlockContainer* __restrict__ blockContainer = dev_VBC[celli];
    if (blockContainer==0) {
@@ -167,7 +170,7 @@ __global__ void second_moments_kernel (
    const Real* __restrict__ blockParameters = blockContainer->getParameters();
 
    const Real HALF = 0.5;
-   const int indx = cellIndex(i,j,k);
+   const int indx = ti%WID3;
 
    // index +0 is number density
    const Real averageVX = dev_moments1[celli*nMom1 + 1];
@@ -175,38 +178,70 @@ __global__ void second_moments_kernel (
    const Real averageVZ = dev_moments1[celli*nMom1 + 3];
 
    for (uint blockIndex = strideOffset; blockIndex < thisVBCSize; blockIndex += stride) {
-      const Realf f = data[blockIndex*WID3+indx];
+      const Realf cellValue = data[blockIndex*WID3+indx];
       const Real* blockParamsZ = &blockParameters[blockIndex*BlockParams::N_VELOCITY_BLOCK_PARAMS];
-      const Real DV3 = blockParamsZ[BlockParams::DVX]*blockParamsZ[BlockParams::DVY]*blockParamsZ[BlockParams::DVZ];
-      const Real VX = blockParamsZ[BlockParams::VXCRD] + (i+HALF)*blockParamsZ[BlockParams::DVX];
-      const Real VY = blockParamsZ[BlockParams::VYCRD] + (j+HALF)*blockParamsZ[BlockParams::DVY];
-      const Real VZ = blockParamsZ[BlockParams::VZCRD] + (k+HALF)*blockParamsZ[BlockParams::DVZ];
-      myMom[0] += f * (VX - averageVX) * (VX - averageVX) * DV3;
-      myMom[1] += f * (VY - averageVY) * (VY - averageVY) * DV3;
-      myMom[2] += f * (VZ - averageVZ) * (VZ - averageVZ) * DV3;
-      myMom[3] += f * (VY - averageVY) * (VZ - averageVZ) * DV3;
-      myMom[4] += f * (VX - averageVX) * (VZ - averageVZ) * DV3;
-      myMom[5] += f * (VX - averageVX) * (VY - averageVY) * DV3;
+
+      const Real paramX = blockParamsZ[BlockParams::DVX];
+      const Real paramY = blockParamsZ[BlockParams::DVY];
+      const Real paramZ = blockParamsZ[BlockParams::DVZ];
+
+      const Real DV3 = paramX*paramY*paramZ;
+
+      const Real VX = blockParamsZ[BlockParams::VXCRD] + (i+HALF)*paramX;
+      const Real VY = blockParamsZ[BlockParams::VYCRD] + (j+HALF)*paramY;
+      const Real VZ = blockParamsZ[BlockParams::VZCRD] + (k+HALF)*paramZ;
+
+      const Real VXDifference = VX - averageVX;
+      const Real VYDifference = VY - averageVY;
+      const Real VZDifference = VZ - averageVZ;
+      
+      const Real f = (Real)cellValue;
+      const Real fDV3 = f * DV3;
+
+      const Real fDV3VXDifference = fDV3 * VXDifference;
+      const Real fDV3VYDifference = fDV3 * VYDifference;
+
+      myMom[0] += fDV3VXDifference * VXDifference;
+      myMom[1] += fDV3VYDifference * VYDifference;
+      myMom[2] += fDV3 * VZDifference * VZDifference;
+      myMom[3] += fDV3VYDifference * VZDifference;
+      myMom[4] += fDV3VXDifference * VZDifference;
+      myMom[5] += fDV3VXDifference * VYDifference;
    }
-   __syncthreads();
+
+   const int indexInsideWarp = ti % GPUTHREADS;
+   const int warpIndex = ti / GPUTHREADS;
+
    //Now reduce one-by-one for cell
-   for (uint imom=0; imom<nMom2; imom++) {
-      smom[ti] = myMom[imom];
-      for (unsigned int s=blockSize/2; s>0; s>>=1) {
-         __syncthreads();
-         if (ti < s) {
-            smom[ti] += smom[ti + s];
-         }
+   for (int offset = GPUTHREADS/2; offset > 0; offset /= 2) {
+      for (uint imom=0; imom<nMom2; imom++) {
+         myMom[imom] += gpuKernelShflDown(myMom[imom], offset);
       }
-      __syncthreads();
-      if (ti==0) {
-         if (stride == 1) {
-            dev_moments2[celli*nMom2 + imom] = smom[0];
+   }
+
+   if (indexInsideWarp == 0) {
+      for (uint imom=0; imom<nMom2; imom++) {
+         smom[warpIndex*nMom2+imom] = myMom[imom];
+      }
+   }
+
+   __syncthreads();
+
+   const int warpsPerBlock = blockSize/GPUTHREADS;
+
+   for (uint imom=warpIndex; imom<nMom2; imom +=warpsPerBlock ) {
+      myMom[imom] = (indexInsideWarp < warpsPerBlock) ? smom[indexInsideWarp*nMom2+imom] : 0.0;
+      for (int offset = (warpsPerBlock)/2; offset > 0; offset /= 2) {
+         myMom[imom] += gpuKernelShflDown(myMom[imom], offset);
+      }
+
+      if (indexInsideWarp == 0) {
+         if (gridDim.y == 1) {
+            dev_moments2[celli*nMom2 + imom] = myMom[imom];
          } else {
-            atomicAdd(&dev_moments2[celli*nMom2 + imom],smom[0]);
+            atomicAdd(&dev_moments2[celli*nMom2 + imom],myMom[imom]);
          }
       }
-      __syncthreads();
    }
 }
 
@@ -373,15 +408,19 @@ void gpu_calculateMoments_R(
          continue;
       }
       // Launch kernel calculating this species' contribution to second velocity moments
-      dim3 blockSize(WID,WID,WID);
-      vmesh::LocalID maxVmeshLaunch = sqrt(maxVmeshSizes.at(popID));
+      vmesh::LocalID maxVmeshLaunch = 1;//sqrt(maxVmeshSizes.at(popID));
       maxVmeshLaunch = maxVmeshLaunch < 1 ? 1 : maxVmeshLaunch;
-      dim3 gridSize(nAllCells,maxVmeshLaunch,1);
-      second_moments_kernel<<<gridSize, blockSize, 0, 0>>> (
+
+      uint wid3PerBlockSecondMoments = min(nextPowerOfTwo(maxVmeshLaunch), MAX_WID3_PER_BLOCK);
+      dim3 blockSize(WID,WID,WID*wid3PerBlockSecondMoments);
+      dim3 gridSize(nAllCells,(maxVmeshLaunch+wid3PerBlockSecondMoments-1)/wid3PerBlockSecondMoments,1);
+      int sharedMemorySizeSecondMoments = nMom2 * (WID3 * wid3PerBlockSecondMoments / GPUTHREADS) * sizeof(Real);
+      second_moments_kernel<<<gridSize, blockSize, sharedMemorySizeSecondMoments, 0>>> (
          dev_VBC,
          dev_moments1,
          dev_moments2,
-         nAllCells
+         nAllCells,
+         wid3PerBlockSecondMoments
          );
       CHK_ERR( gpuPeekAtLastError() );
       CHK_ERR( gpuDeviceSynchronize());
@@ -573,15 +612,19 @@ void gpu_calculateMoments_V(
          continue;
       }
       // Launch kernel calculating this species' contribution to second velocity moments
-      dim3 blockSize(WID,WID,WID);
-      vmesh::LocalID maxVmeshLaunch = sqrt(maxVmeshSizes.at(popID));
+      vmesh::LocalID maxVmeshLaunch = 1;//sqrt(maxVmeshSizes.at(popID));
       maxVmeshLaunch = maxVmeshLaunch < 1 ? 1 : maxVmeshLaunch;
-      dim3 gridSize(nAllCells,maxVmeshLaunch,1);
-      second_moments_kernel<<<gridSize, blockSize, 0, 0>>> (
+
+      uint wid3PerBlockSecondMoments = min(nextPowerOfTwo(maxVmeshLaunch), MAX_WID3_PER_BLOCK);
+      dim3 blockSize(WID,WID,WID*wid3PerBlockSecondMoments);
+      dim3 gridSize(nAllCells,(maxVmeshLaunch+wid3PerBlockSecondMoments-1)/wid3PerBlockSecondMoments,1);
+      int sharedMemorySizeSecondMoments = nMom2 * (WID3 * wid3PerBlockSecondMoments / GPUTHREADS) * sizeof(Real);
+      second_moments_kernel<<<gridSize, blockSize, sharedMemorySizeSecondMoments, 0>>> (
          dev_VBC,
          dev_moments1,
          dev_moments2,
-         nAllCells
+         nAllCells,
+         wid3PerBlockSecondMoments
          );
       CHK_ERR( gpuPeekAtLastError() );
       CHK_ERR( gpuDeviceSynchronize());
