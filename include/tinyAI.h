042b22f509324133eaec5c0ae414908075222822
kstppd <kpapadakis@protonmail.com>
2026-05-08 09:54:33 +0300

    update loss calc

--- include/tinyAI.h
+++ include/tinyAI.h
@@ -645,14 +645,13 @@ public:
       NumericMatrix::loss<T, LOSSFUNCTION>(layers.back()->a, target, error, &handle, stream);
       T loss = T(0.0);
       if constexpr (Backend == BACKEND::HOST) {
-         loss += NumericMatrix::matreduce_add(error, &handle);
+         loss = NumericMatrix::matreduce_add(error, &handle);
       } else {
-         loss += NumericMatrix::matreduce_add_gpu(error, _pool, &handle, stream);
+         loss = NumericMatrix::matreduce_add_gpu(error, _pool, &handle, stream);
       }
       tinyAI_gpuStreamSynchronize(stream);
-      return loss;
+      return loss / static_cast<T>(target.nrows() * target.ncols());
    }
-
    template <BACKEND HW, LOSSF LOSSFUNCTION>
    T loss(NumericMatrix::Matrix<T, HW>& error, NumericMatrix::Matrix<T, HW>& target, tinyAI_gpuStream_t stream = 0) {
       NumericMatrix::ConstMatrixView<T> target_view;
